6a4a92d0755e35099ae1ae7933dc6b9d29e2d668
Minal-Matai <mmatai@madeira.org>
2025-10-21 09:06:19 -0400

    Explores read and readline methods to the file.

---
+++ schedule.py
@@ -0,0 +1,17 @@
+
+
+my_schedule = open("schedule.txt")
+
+print(my_schedule)
+
+#print(my_schedule.read()) #prints out the entire file, preserves spacing and formatting
+
+#print(my_schedule.read(3)) #if you put a number in the (), it prints out that num of charecteristics from the file
+
+print(my_schedule.readline()) #Prints out a full line of the file
+
+my_schedule.readline()
+print(my_schedule.read(3))
+
+
+
