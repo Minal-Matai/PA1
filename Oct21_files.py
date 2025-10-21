f44a65a56dbf2197d7297055fc53e6d5699171f1
Minal-Matai <mmatai@madeira.org>
2025-10-21 09:50:59 -0400

    Tested File Methods

--- Oct21_files.py
+++ Oct21_files.py
@@ -1,5 +1,19 @@
-my_schedule = open("schedule.txt")
+#my_schedule = open("schedule.txt") #By default opens in read mode, can open in write or append mode
+my_schedule = open("schedule.txt", "w") #write mode ...
+my_schedule.write("Changing content!") #... overwrites all files content
+my_schedule=open("schedule.txt", "a") #append mode...
+my_schedule.write("\nAdding content!") #...adds to the end of the file contents           
 
+'''
+To create a new file in python, just open one that doesn't exist in an edit mode or "x" mode.
+'''
+#new_file = open("new_file.txt","x") #just creates new file
+#new_file.write("HI")
+
+new_file = open("new_file.txt","w") #just creates new file
+new_file.write("HI")
+
+'''
 line = my_schedule.readline()
 
 while line != "Mod 2:\n":
@@ -8,4 +22,4 @@ print(line) #Print the mod 2 label
 
 for i in range(3): #now we are at the right place so...
    print(my_schedule.readline()) #print the next 3 lines
-   
+'''
